2a5c2c1734aac9cec47d68d9dc54c670cb458bbc
Dušan Reljić <reljicd@gmail.com>
2023-03-08 12:34:40 +0100

    Changed default immudb database

--- logger_api/app/immudb/config/env_vars.py
+++ logger_api/app/immudb/config/env_vars.py
@@ -2,6 +2,6 @@ import os
 
 IMMUDB_HOST: str = os.getenv(key='IMMUDB_HOST', default='localhost')
 IMMUDB_PORT: int = int(os.getenv(key='IMMUDB_PORT', default='3322'))
-IMMUDB_DB: str = os.getenv(key='IMMUDB_DB', default='defaultdb')
+IMMUDB_DB: str = os.getenv(key='IMMUDB_DB', default='logger')
 IMMUDB_USER: str = os.getenv(key='IMMUDB_USER', default='immudb')
 IMMUDB_PASSWORD: str = os.getenv(key='IMMUDB_PASSWORD', default='password')
